a9a45dffc4edfdfb0265dc6cd8de9cf3e8559d55
Alexandra <asteine8@gmail.com>
2021-08-23 15:51:52 -0700

    Initial Commit

---
+++ HelloWorld2/src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int i = 0;
+        for (;i<10;i++) {
+            System.out.println(i);
+        }
+    }
+}
